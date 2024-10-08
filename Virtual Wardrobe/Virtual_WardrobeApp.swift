2fc53c64569a0c1325d5813e0144b6d66306117d
Azuany Mila Ceron <azuany.mila@gmail.com>
2024-10-08 14:29:08 +0200

    Initial Commit

---
+++ Virtual Wardrobe/Virtual_WardrobeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Virtual_WardrobeApp.swift
+//  Virtual Wardrobe
+//
+//  Created by Azuany Mila on 08/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Virtual_WardrobeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
