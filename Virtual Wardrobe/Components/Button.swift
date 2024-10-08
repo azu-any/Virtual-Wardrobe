804cd731a35a14ea908f905b14f6b02a1e24b347
Azuany Mila Ceron <azuany.mila@gmail.com>
2024-10-08 17:25:04 +0200

    components

---
+++ Virtual Wardrobe/Components/Button.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Virtual Wardrobe
+//
+//  Created by Azuany Mila on 08/10/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
