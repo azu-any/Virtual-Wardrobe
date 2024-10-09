a862437e617d95634cd8500eef6c1602abad46fa
azu-any <azuany.mila@gmail.com>
2024-10-09 15:59:04 +0200

    calendar

--- Virtual Wardrobe/Calendar/Calendar.swift
+++ Virtual Wardrobe/Calendar/Calendar.swift
@@ -5,10 +5,28 @@
 //  Created by Azuany Mila on 08/10/24.
 //
 
+
 import SwiftUI
 
-public struct Calendar {
-    public init() {
-            
+struct CalendarView: View {
+    var body: some View {
+        NavigationStack{
+            VStack{
+                
+                DatePicker(selection: /*@START_MENU_TOKEN@*/.constant(Date())/*@END_MENU_TOKEN@*/, label: { /*@START_MENU_TOKEN@*/Text("Date")/*@END_MENU_TOKEN@*/ })
+                    .datePickerStyle(.graphical)
+
+            }
+            .navigationTitle(Text("Calendar"))
+        }
+       
+        
+        
+        
     }
+}
+
+#Preview(traits: .sizeThatFitsLayout) {
+    CalendarView()
+        .padding()
 }
